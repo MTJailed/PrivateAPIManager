9cd0809a166ccbe847f84539e21f78e964e385b3
MTJailed <marketing@jailed.ml>
2017-12-21 13:57:29 +0100

    Create APIManager.m

---
+++ APIManager.m
@@ -0,0 +1,6 @@
+/*
+  Created by Sem Voigtländer
+  Reverse engineering Apple Private APIs is allowed by Apple.
+  Using code reverse engineered from Private APIs is not recommended as the original code might be licensed.
+  Providing code reverse engineered from Private APIs might infringe DMCA rights.
+*/
