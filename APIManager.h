f65b477a4160d0a283b7543c5b73449de5ffb494
MTJailed <marketing@jailed.ml>
2017-12-21 14:38:00 +0100

    Update APIManager.h

--- APIManager.h
+++ APIManager.h
@@ -1,4 +1,14 @@
-#include <Foundation/Foundation.h>
+#ifndef APIManager_h
+#define APIManager_h
+#import <Foundation/Foundation.h>
 
 @interface APIManager : NSObject
++(void)dumpAllFromFramework:(NSString*)framework privateFW:(BOOL)pfw;
++(void)dumpClasses;
++(NSArray*)dumpClasses:(NSString*)framework privateFW:(BOOL)pfw;
++(void)dumpMethods:(Class)class;
++(void)dumpProperties:(Class)class;
++(BOOL)loadFW:(NSString*)name;
++(BOOL)loadPFW:(NSString*)name;
 @end
+#endif /* APIManager_h */
